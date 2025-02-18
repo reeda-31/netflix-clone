3345d142053c40cad76947151029a1e1c42476da
Syed Reeda Mehwesh <mehweshreeda31@gmail.com>
2025-02-18 17:37:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+var btn1=document.getElementsByClassName("btn1")[0];
+function next()
+{
+    window.location.href="signin.html"
+}
+var flag=0;
+var el1=document.getElementsByClassName("p21")[0];
+function rotate1()
+{
+    if(flag==0){el1.style.rotate="45deg";flag=1;
+    }
+    else
+    {
+        el1.style.rotate="90deg";flag=0;
+    }
+}
+    
+var el2=document.getElementsByClassName("p22")[0];
+function rotate2()
+{
+    if(flag==0){el2.style.rotate="45deg";flag=1;
+    }
+    else
+    {
+        el2.style.rotate="90deg";flag=0;
+    }
+}
+var el3=document.getElementsByClassName("p23")[0];
+function rotate3()
+{
+    if(flag==0){el3.style.rotate="45deg";flag=1;
+    }
+    else
+    {
+        el3.style.rotate="90deg";flag=0;
+    }
+}
+var el4=document.getElementsByClassName("p24")[0];
+function rotate4()
+{
+    if(flag==0){el4.style.rotate="45deg";flag=1;
+    }
+    else
+    {
+        el4.style.rotate="90deg";flag=0;
+    }
+}
+var el5=document.getElementsByClassName("p25")[0];
+function rotate5()
+{
+    if(flag==0){el5.style.rotate="45deg";flag=1;
+    }
+    else
+    {
+        el5.style.rotate="90deg";flag=0;
+    }
+}
+var el6=document.getElementsByClassName("p26")[0];
+function rotate6()
+{
+    if(flag==0){el6.style.rotate="45deg";flag=1;
+    }
+    else
+    {
+        el6.style.rotate="90deg";flag=0;
+    }
+}
